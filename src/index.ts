c63fb504ec1fb6a1e2f499ada4042bce7cfba0d8
Jordan Hansen <jbhansen84@gmail.com>
2020-06-24 18:55:07 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,88 @@
+import axios, { AxiosResponse } from 'axios';
+import cheerio from 'cheerio';
+import { tableNames } from './tableNames';
+import * as json2csv from 'json2csv';
+import * as fs from 'fs';
+
+
+(async () => {
+	const allFields: any[] = [];
+
+	// for (let i = 0; i < tableNames.length; i++) {
+	for (let i = 0; i < 10; i++) {
+		const values = await getTableValues(tableNames[i].SAP_Table);
+
+		allFields.push(...values);
+
+		console.log('values', allFields.length);
+
+		await timeout(1000);
+	}
+
+	const csv = json2csv.parse(allFields);
+	const fileName = 'Table fields.csv';
+	fs.writeFile(fileName, csv, (err) => {
+		if (err) {
+			console.log('error saving file', err);
+		}
+	})
+
+
+})();
+
+async function getTableValues(tableName: string) {
+	const url = `http://leanx.eu/en/sap/table/${tableName}.html`;
+	console.log('Searching - ', url);
+
+	let axiosResponse: AxiosResponse;
+
+	try {
+		axiosResponse = await axios.get(url);
+	}
+	catch (e) {
+		console.log('Error searching ', url, e.response ? e.response.status : '');
+		return [];
+	}
+	const fieldValues: any = [];
+
+	const $ = cheerio.load(axiosResponse.data);
+
+	const rows = $('h3:nth-of-type(1) + .table-responsive > table > tbody > tr');
+
+	for (let i = 0; i < rows.length; i++) {
+		const row$ = cheerio.load(rows[i]);
+		const field = row$('td:nth-of-type(1)').first().text();
+
+		const possibleValues = row$('.collapse tr');
+
+		if (possibleValues.length > 0) {
+			for (let possibleValueIndex = 0; possibleValueIndex < possibleValues.length; possibleValueIndex++) {
+				const possibleValue$ = cheerio.load(possibleValues[possibleValueIndex]);
+				const possibleValue = possibleValue$('td:nth-of-type(2)').text();
+
+				fieldValues.push({
+					table: tableName,
+					field: field,
+					value: possibleValue
+				});
+
+			}
+		}
+		else {
+			fieldValues.push({
+				table: tableName,
+				field: field,
+				value: field
+			});
+		}
+	}
+
+	console.log('Found ', fieldValues.length, ' fields.');
+	return fieldValues;
+}
+
+
+function timeout(ms: number) {
+	return new Promise(res => setTimeout(res, ms));
+}
+
---
+++ src/tableNames.ts
@@ -0,0 +1,1001 @@
+export const tableNames = [
+	{
+		"SAP_Table": "HLVL_CCTRG"
+	},
+	{
+		"SAP_Table": "EKES"
+	},
+	{
+		"SAP_Table": "MARD"
+	},
+	{
+		"SAP_Table": "RCE29P"
+	},
+	{
+		"SAP_Table": "/sapapo/matmap"
+	},
+	{
+		"SAP_Table": "BNKA"
+	},
+	{
+		"SAP_Table": "MARK02"
+	},
+	{
+		"SAP_Table": "Operations and Components"
+	},
+	{
+		"SAP_Table": "WYT3"
+	},
+	{
+		"SAP_Table": "DMS_LODESC_CD1"
+	},
+	{
+		"SAP_Table": "bapiscmb0008zdescr"
+	},
+	{
+		"SAP_Table": "DFKKBPTAXNUM"
+	},
+	{
+		"SAP_Table": "QMTT"
+	},
+	{
+		"SAP_Table": "CSLA"
+	},
+	{
+		"SAP_Table": "CAUFVD"
+	},
+	{
+		"SAP_Table": "T353I"
+	},
+	{
+		"SAP_Table": "LIPS"
+	},
+	{
+		"SAP_Table": "LLVL_PCTR_TO"
+	},
+	{
+		"SAP_Table": "EKKN"
+	},
+	{
+		"SAP_Table": "IPHA"
+	},
+	{
+		"SAP_Table": "CABN"
+	},
+	{
+		"SAP_Table": "B010"
+	},
+	{
+		"SAP_Table": "HLVL_PCTRH"
+	},
+	{
+		"SAP_Table": "AFAB"
+	},
+	{
+		"SAP_Table": "EKET"
+	},
+	{
+		"SAP_Table": "BUT0BK"
+	},
+	{
+		"SAP_Table": "/PLMI/ECR_CI_LBL"
+	},
+	{
+		"SAP_Table": "PLPOD"
+	},
+	{
+		"SAP_Table": "CSKA"
+	},
+	{
+		"SAP_Table": "VIQMEL"
+	},
+	{
+		"SAP_Table": "KNA1"
+	},
+	{
+		"SAP_Table": "QMEL"
+	},
+	{
+		"SAP_Table": "/PLMB/RCP_SPEC"
+	},
+	{
+		"SAP_Table": "MCHB"
+	},
+	{
+		"SAP_Table": "/SAPAPO/LOCIO"
+	},
+	{
+		"SAP_Table": "DIIHPA"
+	},
+	{
+		"SAP_Table": "CSKS"
+	},
+	{
+		"SAP_Table": "Header Data (mandatory)"
+	},
+	{
+		"SAP_Table": "A304"
+	},
+	{
+		"SAP_Table": "IMPTT"
+	},
+	{
+		"SAP_Table": "MEPO_TOPLINE"
+	},
+	{
+		"SAP_Table": "QPMT"
+	},
+	{
+		"SAP_Table": "MARC"
+	},
+	{
+		"SAP_Table": "VBEP"
+	},
+	{
+		"SAP_Table": "LFM1"
+	},
+	{
+		"SAP_Table": "RIWO00"
+	},
+	{
+		"SAP_Table": "RILO0"
+	},
+	{
+		"SAP_Table": "PLMKB"
+	},
+	{
+		"SAP_Table": "MEX"
+	},
+	{
+		"SAP_Table": "VBAK"
+	},
+	{
+		"SAP_Table": "Line Item"
+	},
+	{
+		"SAP_Table": "HLVL_PCTRG"
+	},
+	{
+		"SAP_Table": "ESLL"
+	},
+	{
+		"SAP_Table": "bapiscmb0008zonelanguiso"
+	},
+	{
+		"SAP_Table": "MSEG"
+	},
+	{
+		"SAP_Table": "LIKP"
+	},
+	{
+		"SAP_Table": "ESTST"
+	},
+	{
+		"SAP_Table": "RM06I"
+	},
+	{
+		"SAP_Table": "BUTC01_TC"
+	},
+	{
+		"SAP_Table": "BSEG"
+	},
+	{
+		"SAP_Table": "bapi10004commctrl"
+	},
+	{
+		"SAP_Table": "MMPT"
+	},
+	{
+		"SAP_Table": "VIQMFE"
+	},
+	{
+		"SAP_Table": "/sapapo/matexec"
+	},
+	{
+		"SAP_Table": "/SCTM/V_PFG"
+	},
+	{
+		"SAP_Table": "MLAN"
+	},
+	{
+		"SAP_Table": "KNKA"
+	},
+	{
+		"SAP_Table": "/SAPAPO/TRM"
+	},
+	{
+		"SAP_Table": "KAKO"
+	},
+	{
+		"SAP_Table": "/PLMB/RCP_RECIPE"
+	},
+	{
+		"SAP_Table": "CAWN"
+	},
+	{
+		"SAP_Table": "A"
+	},
+	{
+		"SAP_Table": "/sapapo/matlwhst"
+	},
+	{
+		"SAP_Table": "MAPL"
+	},
+	{
+		"SAP_Table": "bapiscmb0008zonelangu"
+	},
+	{
+		"SAP_Table": "ESUH"
+	},
+	{
+		"SAP_Table": "/SCMTMS/C_TCET"
+	},
+	{
+		"SAP_Table": "RMCLM"
+	},
+	{
+		"SAP_Table": "EINA"
+	},
+	{
+		"SAP_Table": "/SAPAPO/TR"
+	},
+	{
+		"SAP_Table": "SWOR"
+	},
+	{
+		"SAP_Table": "/SCMTMS/D_TCCSRT"
+	},
+	{
+		"SAP_Table": "MARK04"
+	},
+	{
+		"SAP_Table": "AFKO"
+	},
+	{
+		"SAP_Table": "Storage Category"
+	},
+	{
+		"SAP_Table": "ESTRI"
+	},
+	{
+		"SAP_Table": "CEPCT"
+	},
+	{
+		"SAP_Table": "bapiscmblogsys"
+	},
+	{
+		"SAP_Table": "MPLA"
+	},
+	{
+		"SAP_Table": "CEPC"
+	},
+	{
+		"SAP_Table": "Default =C or L"
+	},
+	{
+		"SAP_Table": "ANKA"
+	},
+	{
+		"SAP_Table": "SKA1"
+	},
+	{
+		"SAP_Table": "KONH"
+	},
+	{
+		"SAP_Table": "Part 1, Part 2"
+	},
+	{
+		"SAP_Table": "AFRV"
+	},
+	{
+		"SAP_Table": "LLVL_PCTR"
+	},
+	{
+		"SAP_Table": "/SAPAPO/LOC"
+	},
+	{
+		"SAP_Table": "MAST"
+	},
+	{
+		"SAP_Table": "B025"
+	},
+	{
+		"SAP_Table": "SAP Table"
+	},
+	{
+		"SAP_Table": "EKKO_TEXT"
+	},
+	{
+		"SAP_Table": "SAG Header Data (mandatory)"
+	},
+	{
+		"SAP_Table": "Beginning, Middle, End"
+	},
+	{
+		"SAP_Table": "LLVL_CCTR"
+	},
+	{
+		"SAP_Table": "KNVP"
+	},
+	{
+		"SAP_Table": "MPOP"
+	},
+	{
+		"SAP_Table": "KOTG001"
+	},
+	{
+		"SAP_Table": "KONP"
+	},
+	{
+		"SAP_Table": "/PLMB/RCP_PLNT"
+	},
+	{
+		"SAP_Table": "KONDDP"
+	},
+	{
+		"SAP_Table": "TCCLG"
+	},
+	{
+		"SAP_Table": "PMCOSUM"
+	},
+	{
+		"SAP_Table": "/PLMB/RCP_ASSIGN"
+	},
+	{
+		"SAP_Table": "Configurable"
+	},
+	{
+		"SAP_Table": "AFIH"
+	},
+	{
+		"SAP_Table": "ESUC"
+	},
+	{
+		"SAP_Table": "KNBK"
+	},
+	{
+		"SAP_Table": "Item Data (mandatory)"
+	},
+	{
+		"SAP_Table": "Material Document Header (mandatory)"
+	},
+	{
+		"SAP_Table": "EQUZ"
+	},
+	{
+		"SAP_Table": "/SCWM/LAGP"
+	},
+	{
+		"SAP_Table": "TCA01"
+	},
+	{
+		"SAP_Table": "LLVL_CCTR_TO"
+	},
+	{
+		"SAP_Table": "CAWNT"
+	},
+	{
+		"SAP_Table": "GL Account Master Chart of accounts Level"
+	},
+	{
+		"SAP_Table": "HLVL_CCTRH"
+	},
+	{
+		"SAP_Table": "RESBD"
+	},
+	{
+		"SAP_Table": "Material Document Item (mandatory)"
+	},
+	{
+		"SAP_Table": "/PLMB/RFO_FRM"
+	},
+	{
+		"SAP_Table": "VBKD"
+	},
+	{
+		"SAP_Table": "CSKB"
+	},
+	{
+		"SAP_Table": "KNVA"
+	},
+	{
+		"SAP_Table": "T351"
+	},
+	{
+		"SAP_Table": "KNBW"
+	},
+	{
+		"SAP_Table": "bapiscmblogqs"
+	},
+	{
+		"SAP_Table": "LLVL_COAREA"
+	},
+	{
+		"SAP_Table": "T001W"
+	},
+	{
+		"SAP_Table": "MAKT"
+	},
+	{
+		"SAP_Table": "Class"
+	},
+	{
+		"SAP_Table": "QPMZ"
+	},
+	{
+		"SAP_Table": "AFPO"
+	},
+	{
+		"SAP_Table": "ANLB"
+	},
+	{
+		"SAP_Table": "PLPS"
+	},
+	{
+		"SAP_Table": "KNVI"
+	},
+	{
+		"SAP_Table": "/PLMB/RFO_FRMITM"
+	},
+	{
+		"SAP_Table": "ADDR1_DATA"
+	},
+	{
+		"SAP_Table": "IHPA"
+	},
+	{
+		"SAP_Table": "T418V"
+	},
+	{
+		"SAP_Table": "/SCMB/S_UI_BPTMSCAC_VEN_TC"
+	},
+	{
+		"SAP_Table": "/SCWM/BINMAT"
+	},
+	{
+		"SAP_Table": "ANIA"
+	},
+	{
+		"SAP_Table": "KONDD"
+	},
+	{
+		"SAP_Table": "VBPA"
+	},
+	{
+		"SAP_Table": "CABNT"
+	},
+	{
+		"SAP_Table": "MAKTX"
+	},
+	{
+		"SAP_Table": "STKO"
+	},
+	{
+		"SAP_Table": "MARK03"
+	},
+	{
+		"SAP_Table": "MARK01"
+	},
+	{
+		"SAP_Table": "VBAP"
+	},
+	{
+		"SAP_Table": "RCTMV"
+	},
+	{
+		"SAP_Table": "ITOB"
+	},
+	{
+		"SAP_Table": "MKPF"
+	},
+	{
+		"SAP_Table": "Document Header"
+	},
+	{
+		"SAP_Table": "CRHD"
+	},
+	{
+		"SAP_Table": "RIMR0"
+	},
+	{
+		"SAP_Table": "RESB"
+	},
+	{
+		"SAP_Table": "BUT000"
+	},
+	{
+		"SAP_Table": "Related to storage of document itself"
+	},
+	{
+		"SAP_Table": "EAPL"
+	},
+	{
+		"SAP_Table": "KOTH001"
+	},
+	{
+		"SAP_Table": "IMPR"
+	},
+	{
+		"SAP_Table": "KLAT"
+	},
+	{
+		"SAP_Table": "PLKO"
+	},
+	{
+		"SAP_Table": "KSML"
+	},
+	{
+		"SAP_Table": "STPO"
+	},
+	{
+		"SAP_Table": "P3005"
+	},
+	{
+		"SAP_Table": "ITOBATTR"
+	},
+	{
+		"SAP_Table": "QMMA"
+	},
+	{
+		"SAP_Table": "EINE"
+	},
+	{
+		"SAP_Table": "ADR2"
+	},
+	{
+		"SAP_Table": "EKKO"
+	},
+	{
+		"SAP_Table": "ANLA"
+	},
+	{
+		"SAP_Table": "Account Assignment"
+	},
+	{
+		"SAP_Table": "bapiscmb0008zoneregio"
+	},
+	{
+		"SAP_Table": "Create Statistical Key Figures: Master Data"
+	},
+	{
+		"SAP_Table": "Delivery Address"
+	},
+	{
+		"SAP_Table": "EQST"
+	},
+	{
+		"SAP_Table": "RC271"
+	},
+	{
+		"SAP_Table": "T007V"
+	},
+	{
+		"SAP_Table": "KOTN010"
+	},
+	{
+		"SAP_Table": "ZWP"
+	},
+	{
+		"SAP_Table": "QPMK"
+	},
+	{
+		"SAP_Table": "AUSP"
+	},
+	{
+		"SAP_Table": "AUFK"
+	},
+	{
+		"SAP_Table": "MARM"
+	},
+	{
+		"SAP_Table": "SKB1"
+	},
+	{
+		"SAP_Table": "PLPO"
+	},
+	{
+		"SAP_Table": "RQM02"
+	},
+	{
+		"SAP_Table": "CRTX"
+	},
+	{
+		"SAP_Table": "Enter Statistical Key Figures"
+	},
+	{
+		"SAP_Table": "CLASSLONGTEXTS"
+	},
+	{
+		"SAP_Table": "Defined by the business"
+	},
+	{
+		"SAP_Table": "ANLC"
+	},
+	{
+		"SAP_Table": "/SCMTMS/C_CBASET"
+	},
+	{
+		"SAP_Table": "KNVV"
+	},
+	{
+		"SAP_Table": "ADR6"
+	},
+	{
+		"SAP_Table": "T002"
+	},
+	{
+		"SAP_Table": "QMSM"
+	},
+	{
+		"SAP_Table": "DMS_PH_CD1"
+	},
+	{
+		"SAP_Table": "QMAT"
+	},
+	{
+		"SAP_Table": "Can have multiple classes per doc"
+	},
+	{
+		"SAP_Table": "EORD"
+	},
+	{
+		"SAP_Table": "BUT100"
+	},
+	{
+		"SAP_Table": "MDKPDB"
+	},
+	{
+		"SAP_Table": "HLVL_COAREA"
+	},
+	{
+		"SAP_Table": "MMPX"
+	},
+	{
+		"SAP_Table": "MKAL"
+	},
+	{
+		"SAP_Table": "MVKE"
+	},
+	{
+		"SAP_Table": "ANLP"
+	},
+	{
+		"SAP_Table": "EKPA"
+	},
+	{
+		"SAP_Table": "LLVL_CCTRG"
+	},
+	{
+		"SAP_Table": "/SCMTMS/TCM_RATE2_TABLES"
+	},
+	{
+		"SAP_Table": "IFLOT"
+	},
+	{
+		"SAP_Table": "T351T"
+	},
+	{
+		"SAP_Table": "EQUI"
+	},
+	{
+		"SAP_Table": "STXH"
+	},
+	{
+		"SAP_Table": ": Master BOM, Sub-BOM"
+	},
+	{
+		"SAP_Table": "/AENR"
+	},
+	{
+		"SAP_Table": "COBK"
+	},
+	{
+		"SAP_Table": "/SCTM/V_GAV"
+	},
+	{
+		"SAP_Table": "UKMBP_CMS"
+	},
+	{
+		"SAP_Table": "DRAW"
+	},
+	{
+		"SAP_Table": "MCH1"
+	},
+	{
+		"SAP_Table": "/sapapo/matlwh"
+	},
+	{
+		"SAP_Table": "ILOA"
+	},
+	{
+		"SAP_Table": "MPOS"
+	},
+	{
+		"SAP_Table": "RSTXT"
+	},
+	{
+		"SAP_Table": "DRAT"
+	},
+	{
+		"SAP_Table": "UKMBP_CMS_SGM"
+	},
+	{
+		"SAP_Table": "BKPF"
+	},
+	{
+		"SAP_Table": "BUT021"
+	},
+	{
+		"SAP_Table": "/SCMTMS/C_TCCATT"
+	},
+	{
+		"SAP_Table": "CSLT"
+	},
+	{
+		"SAP_Table": "/SCMTMS/V_DIMWP"
+	},
+	{
+		"SAP_Table": "bapiscmb0008zoneid"
+	},
+	{
+		"SAP_Table": "ALM_ME_C014DEF"
+	},
+	{
+		"SAP_Table": "COMPONENTS"
+	},
+	{
+		"SAP_Table": "Vendor Address"
+	},
+	{
+		"SAP_Table": "SPTO"
+	},
+	{
+		"SAP_Table": "CRCO"
+	},
+	{
+		"SAP_Table": "/SAPAPO/CALIO"
+	},
+	{
+		"SAP_Table": "T002T"
+	},
+	{
+		"SAP_Table": "Z"
+	},
+	{
+		"SAP_Table": "KLAH"
+	},
+	{
+		"SAP_Table": "BUT051"
+	},
+	{
+		"SAP_Table": "SZA1_D0100"
+	},
+	{
+		"SAP_Table": "ESTMJ"
+	},
+	{
+		"SAP_Table": "AENR"
+	},
+	{
+		"SAP_Table": "Header (mandatory) VBAK"
+	},
+	{
+		"SAP_Table": "TSKM"
+	},
+	{
+		"SAP_Table": "Filter Criteria"
+	},
+	{
+		"SAP_Table": "LFA1"
+	},
+	{
+		"SAP_Table": "QMFE"
+	},
+	{
+		"SAP_Table": "/PLMI"
+	},
+	{
+		"SAP_Table": "NACH"
+	},
+	{
+		"SAP_Table": "VIQMUR"
+	},
+	{
+		"SAP_Table": "IMPT"
+	},
+	{
+		"SAP_Table": "Fixed Bin Assignment"
+	},
+	{
+		"SAP_Table": "KONDN"
+	},
+	{
+		"SAP_Table": "ANLH"
+	},
+	{
+		"SAP_Table": "BUT050"
+	},
+	{
+		"SAP_Table": "RMCLF"
+	},
+	{
+		"SAP_Table": "SKAT"
+	},
+	{
+		"SAP_Table": "SAG Item Data (mandatory)"
+	},
+	{
+		"SAP_Table": "STZU"
+	},
+	{
+		"SAP_Table": "/SCWM/S_STOCK_UPLOAD_EXT"
+	},
+	{
+		"SAP_Table": "KNKK"
+	},
+	{
+		"SAP_Table": "/SCMTMS/C_TCETT"
+	},
+	{
+		"SAP_Table": "RIHSTPX"
+	},
+	{
+		"SAP_Table": "AFVC"
+	},
+	{
+		"SAP_Table": "T179"
+	},
+	{
+		"SAP_Table": "OPERATIONS"
+	},
+	{
+		"SAP_Table": "/sapapo/matpack"
+	},
+	{
+		"SAP_Table": "Item (mandatory) VBAP"
+	},
+	{
+		"SAP_Table": "COPPP_ITM"
+	},
+	{
+		"SAP_Table": "QMIH"
+	},
+	{
+		"SAP_Table": "BUT0ID"
+	},
+	{
+		"SAP_Table": "KNMT"
+	},
+	{
+		"SAP_Table": "QMUR"
+	},
+	{
+		"SAP_Table": "HEADER"
+	},
+	{
+		"SAP_Table": "KONV"
+	},
+	{
+		"SAP_Table": "KNB1"
+	},
+	{
+		"SAP_Table": "TKA03"
+	},
+	{
+		"SAP_Table": "bapiscmb0008zonectry"
+	},
+	{
+		"SAP_Table": "CVIS_LFBW"
+	},
+	{
+		"SAP_Table": "Additional Header Partners VBPA"
+	},
+	{
+		"SAP_Table": "LFM2"
+	},
+	{
+		"SAP_Table": "GL Account Master Company Code Level"
+	},
+	{
+		"SAP_Table": "T003O"
+	},
+	{
+		"SAP_Table": "EKPO_TEXT"
+	},
+	{
+		"SAP_Table": "ADRC"
+	},
+	{
+		"SAP_Table": "DMS_DOC_FILES"
+	},
+	{
+		"SAP_Table": "Item Partners VBPA"
+	},
+	{
+		"SAP_Table": "/SAPAPO/TRPROD"
+	},
+	{
+		"SAP_Table": "B011"
+	},
+	{
+		"SAP_Table": "PLMK"
+	},
+	{
+		"SAP_Table": "MARA"
+	},
+	{
+		"SAP_Table": "T351P"
+	},
+	{
+		"SAP_Table": "KNB5"
+	},
+	{
+		"SAP_Table": "QMTT_LONGTEXT"
+	},
+	{
+		"SAP_Table": "EQKT"
+	},
+	{
+		"SAP_Table": "MBEW"
+	},
+	{
+		"SAP_Table": "TKA01"
+	},
+	{
+		"SAP_Table": "LFB1"
+	},
+	{
+		"SAP_Table": "ZWI"
+	},
+	{
+		"SAP_Table": "ESTRH"
+	},
+	{
+		"SAP_Table": "/sapapo/matkey"
+	},
+	{
+		"SAP_Table": "/sapapo/marm"
+	},
+	{
+		"SAP_Table": "DIADR"
+	},
+	{
+		"SAP_Table": "LLVL_PCTRG"
+	},
+	{
+		"SAP_Table": "/SCMTMS/D_FAGROT"
+	},
+	{
+		"SAP_Table": "Sample Document Types"
+	},
+	{
+		"SAP_Table": "AFVV"
+	},
+	{
+		"SAP_Table": "Sample Document Part"
+	},
+	{
+		"SAP_Table": "AFVGD"
+	},
+	{
+		"SAP_Table": "B009"
+	},
+	{
+		"SAP_Table": "KNVK"
+	},
+	{
+		"SAP_Table": "QMTB"
+	},
+	{
+		"SAP_Table": "/SCMTMS/C_TCCLST"
+	},
+	{
+		"SAP_Table": "/sapapo/mattxt"
+	},
+	{
+		"SAP_Table": "ZSD"
+	},
+	{
+		"SAP_Table": "EKPO"
+	}
+];
